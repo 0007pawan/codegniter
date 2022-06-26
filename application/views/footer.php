aa6c79eef055c06225ac443e9555933e223230db
0007pawan <pk2261050@gmail.com>
2022-06-26 12:12:02 +0530

    Add Navbar

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,2 +1,6 @@
+
+
+<!-- Add Custom js file -->
+<script src="<?php echo base_url()?>asset/js/custom.js"></script>
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>	</title>
-	<link rel="stylesheet" href="">
+	<!-- add custom css file -->
+	<link rel="stylesheet" href="<?php echo base_url()?>asset/css/style.css">
 	<!-- add Bootstarp css file -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<!-- add bootstrap js file -->
@@ -13,6 +14,7 @@
 	 <link rel="stylesheet" href=
 "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- Add jquery cdn file -->
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
 
